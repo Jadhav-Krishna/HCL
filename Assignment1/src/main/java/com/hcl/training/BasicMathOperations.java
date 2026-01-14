0ff52e5a2f413f8b09ceeb337fa9b36c362a4e41
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-15 01:43:21 +0530

    updated maven compiler version to java 21

--- Assignment1/src/main/java/com/hcl/training/BasicMathOperations.java
+++ Assignment1/src/main/java/com/hcl/training/BasicMathOperations.java
@@ -5,35 +5,48 @@ import java.util.Scanner;
 public class BasicMathOperations {
     
     public static int add(int a, int b) {
-        return a + b;
+        int result = a + b;
+        return result;
     }
     
     public static int subtract(int a, int b) {
-        return a - b;
+        int result = a - b;
+        return result;
     }
     
     public static int multiply(int a, int b) {
-        return a * b;
+        int ans = a * b;
+        return ans;
     }
     
     public static double divide(int a, int b) {
-        return (double) a / b;
+        double division = (double) a / b;
+        return division;
     }
     
     public static int remainder(int a, int b) {
-        return a % b;
+        int rem = a % b;
+        return rem;
     }
     
     public static int square(int a) {
-        return a * a;
+        int sq = a * a;
+        return sq;
     }
     
     public static int cube(int a) {
-        return a * a * a;
+        int c = a * a * a;
+        return c;
     }
     
     public static int absolute(int a) {
-        return Math.abs(a);
+        int abs;
+        if(a < 0) {
+            abs = -a;
+        } else {
+            abs = a;
+        }
+        return abs;
     }
     
     public static void main(String[] args) {
@@ -45,7 +58,8 @@ public class BasicMathOperations {
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
         
-        System.out.println("\nResults:");
+        System.out.println("");
+        System.out.println("Results:");
         System.out.println("Addition: " + add(num1, num2));
         System.out.println("Subtraction: " + subtract(num1, num2));
         System.out.println("Multiplication: " + multiply(num1, num2));
